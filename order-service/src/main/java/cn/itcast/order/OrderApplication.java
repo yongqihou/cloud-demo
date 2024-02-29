89f9656e920dfae709c0d8118ea0fd52a91817f0
hou <hello@hou.cn>
2024-02-29 10:18:13 +0800

    提交

--- order-service/src/main/java/cn/itcast/order/OrderApplication.java
+++ order-service/src/main/java/cn/itcast/order/OrderApplication.java
@@ -20,7 +20,7 @@ public class OrderApplication {
     }
 
 //    @Bean
-//    @LoadBalanced覆盖
+//    @LoadBalanced覆盖更改
 //    public RestTemplate restTemplate() {
 //        return new RestTemplate();
 //    }
